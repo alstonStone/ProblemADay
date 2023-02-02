f058203a8eee590891e881da0c918a8af1ce4a56
Alston Stone <alstonstone0@gmail.com>
2023-02-02 11:31:19 -0500

    PalidromeNdumbers initial commit

---
+++ CodeProblems/src/easy/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package easy;
+
+
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+
+    public int[] numberToDigits(int number){
+
+        //calculate size of number
+        int sizeCount = 0;
+        int tempNumber = number;
+        while(tempNumber >= 1){
+            sizeCount++;
+            tempNumber = tempNumber/10
+
+        }
+    }
+}
