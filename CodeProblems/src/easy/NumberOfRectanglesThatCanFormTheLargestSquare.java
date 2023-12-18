cb7316879c917e0e331bbfe3888c0711cb9151a7
alstonStone <alstonStone0@gmail.com>
2023-12-18 12:03:47 -0500

    solved NumberOfRectanglesThatCanFormTheLargestSquare

---
+++ CodeProblems/src/easy/NumberOfRectanglesThatCanFormTheLargestSquare.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList; // import the ArrayList class
+class NumberOfRectanglesThatCanFormTheLargestSquare{
+    public int countGoodRectangles(int[][] rectangles) {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+
+        for(int[] pair: rectangles){
+            int min = Integer.MAX_VALUE;
+            for(int i: pair){
+                if(i< min){
+                    min = i;
+                }
+            }
+            values.add(min);
+        }
+        int max =0;
+        int maxCount =1;
+        for(Integer i : values){
+            if(i == max){
+                maxCount++;
+            }
+            if(i > max){
+                max = i;
+                maxCount = 1;
+            }
+        }
+        return maxCount;
+    }
+}
