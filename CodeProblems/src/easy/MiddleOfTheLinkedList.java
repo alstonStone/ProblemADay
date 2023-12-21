1dfea825171862643735b96437e048072f8327a8
alstonStone <alstonStone0@gmail.com>
2023-12-21 14:39:30 -0500

    Solved Middle Of The Linked List

---
+++ CodeProblems/src/easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode MiddleOfTheLinkedList(ListNode head) {
+        int middleIndex = getLinkedListSize(head) / 2;
+        return getValueAtIndexInLinkedList(middleIndex, head);
+    }
+
+    public static int getLinkedListSize(ListNode head){
+        int count = 0;
+        while(Objects.nonNull(head)){
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+
+    public static ListNode getValueAtIndexInLinkedList(int index, ListNode head){
+        int value;
+        for(int i=0; i<index; i++){
+            head = head.next;
+        }
+        return head;
+    }
+}
